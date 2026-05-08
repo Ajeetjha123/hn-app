86560258c7241f14e43c68a7fadcf35ca5b829fa
Ajeet Kumar Jha <kumarjhaajeet653@gmail.com>
2026-05-08 16:49:41 +0530

    add authentication routes for user registration, login, and profile retrieval

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
+import authRoutes from "./routes/auth.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use("/api/auth", authRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
